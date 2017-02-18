1411c38c8d3306f0ae6cd5c1ba76e02a4337bfd3
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-02-18 23:08:46 +0530

    fix:Added information from the URL ,deleted Placeholder Json .

--- app/src/main/java/com/example/android/quakereport/MainActivity.java
+++ app/src/main/java/com/example/android/quakereport/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.quakereport;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -7,21 +8,40 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String URL_DOMAIN =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Words> words= QueryUtils.extractEarthQuake();
+        EarthQuakeAsynTask task =new EarthQuakeAsynTask();
+        task.execute(URL_DOMAIN);
 
 
 
-        WordAdapter adapter =new WordAdapter(this,words);
-        ListView listView=(ListView)findViewById(R.id.list);
-        listView.setAdapter(adapter );
 
 
+    }
 
+    private class EarthQuakeAsynTask extends AsyncTask<String, Void, ArrayList>{
+
+        @Override
+        protected ArrayList doInBackground(String... urls) {
+            for(int i=0;i<urls.length;i++) {
+                ArrayList<Words> words = QueryUtils.extractEarthQuake(urls[i]);
+                return words;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList words) {
+            WordAdapter adapter = new WordAdapter(MainActivity.this,words);
+            ListView listview=(ListView)findViewById(R.id.list);
+            listview.setAdapter(adapter);
+
+        }
     }
 }
